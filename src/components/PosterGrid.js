eb023e6c5c339f6568c3a55c05285071fa47b8f3
Willy Liu <arkazain@gmail.com>
2018-04-15 23:08:30 +0200

    🚧 Update PosterGrid to better support mixed types

--- src/components/PosterGrid.js
+++ src/components/PosterGrid.js
@@ -3,32 +3,42 @@ import PropTypes from "prop-types";
 import PosterCard from "./PosterCard";
 import "../css/PosterGrid.scss";
 
-function PosterGrid({ movies, type }) {
-  const getTitle = (movie) => {
-    if (type === "tv") {
-      return movie.name;
-    }
-    return movie.title;
-  };
+function getTitle(movie, type) {
+  // if type isn't explicitly set, check the media_type value instead,
+  // which is present in API calls for search, for example
+  const mediaType = type || movie.media_type;
+  return mediaType === "movie" ? movie.title : movie.name;
+}
 
+function getReleaseDate(movie, type) {
+  const mediaType = type || movie.media_type;
+  return mediaType === "movie" ? movie.release_date : movie.first_air_date;
+}
+
+function PosterGrid({ movies, type }) {
   return (
     <div className="poster-grid">
       {movies.map(movie => (
         <PosterCard
           key={movie.id}
-          linkTo={`/${type}/${movie.id}`}
-          title={getTitle(movie)}
+          linkTo={`/${(type || movie.media_type)}/${movie.id}`}
+          title={getTitle(movie, type)}
           posterPath={movie.poster_path}
-          releaseDate={movie.release_date}
+          releaseDate={getReleaseDate(movie, type)}
         />
       ))}
     </div>
   );
 }
 
+PosterGrid.defaultProps = {
+  type: "",
+};
+
 PosterGrid.propTypes = {
   movies: PropTypes.arrayOf(PropTypes.object).isRequired,
-  type: PropTypes.oneOf(["movie", "tv"]).isRequired,
+  /** If the movies array contains both movies and tv shows, don't set the type prop */
+  type: PropTypes.oneOf(["movie", "tv", ""]),
 };
 
 export default PosterGrid;
