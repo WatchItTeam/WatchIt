cb433d8b65bd5fdb9ac3563de95e579e8822308c
Willy Liu <arkazain@gmail.com>
2018-04-16 13:53:22 +0200

    🚧 Show search query in Searchpage

--- src/components/Searchpage.js
+++ src/components/Searchpage.js
@@ -2,10 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import PosterGrid from "../components/PosterGrid";
 
-function Searchpage({ results }) {
+function Searchpage({ results, query }) {
   return (
     <section id="searchpage" className="container">
-      <h1>Search results</h1>
+      <h1>Search results for {query}</h1>
       <PosterGrid movies={results} />
     </section>
   );
@@ -13,6 +13,7 @@ function Searchpage({ results }) {
 
 Searchpage.propTypes = {
   results: PropTypes.arrayOf(PropTypes.object).isRequired,
+  query: PropTypes.string.isRequired,
 };
 
 export default Searchpage;
