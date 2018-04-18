eb56747fa9d85b2fb5555b8b2184094d47dc87a9
Willy Liu <arkazain@gmail.com>
2018-04-18 22:54:20 +0200

    ⚙️ Disable react/sort-comp

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
         "object-curly-newline": 0,
         "react/forbid-prop-types": 0,
         "react/jsx-filename-extension": 0,
+        "react/sort-comp": 0,
         "jsx-a11y/anchor-is-valid": ["error", {
             components: ["Link"],
             specialLink: ["to"],
