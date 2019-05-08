bbd8350f98e1ead6491252441a4c8cf38fd5d8aa
Willy Liu <arkazain@gmail.com>
2019-05-08 23:29:58 +0200

    Fix auth imports

--- src/Firebase/UserUtils.js
+++ src/Firebase/UserUtils.js
@@ -1,17 +1,17 @@
-import firebase from "./firebase";
+import { auth } from "./firebase";
 
 export function signUp(email, password) {
-  return firebase.auth().createUserWithEmailAndPassword(email, password);
+  return auth.createUserWithEmailAndPassword(email, password);
 }
 
 export function signIn(email, password) {
-  return firebase.auth().signInWithEmailAndPassword(email, password);
+  return auth.signInWithEmailAndPassword(email, password);
 }
 
 export function resetPassword(emailAddress) {
-  return firebase.auth().sendPasswordResetEmail(emailAddress);
+  return auth.sendPasswordResetEmail(emailAddress);
 }
 
 export function signOut() {
-  return firebase.auth().signOut();
+  return auth.signOut();
 }
